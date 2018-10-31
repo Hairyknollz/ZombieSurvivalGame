f4edeac4cab221d7ca24dd59f69de56d8525aaf6
Jon Munro <Jonathan.m.munro@gmail.com>
2018-10-31 18:59:23 +0000

    Redone Zombie

---
+++ FPS attempt/Assets/Scripts/Zombies/ZombieAI.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public HealthMonitor healthMonitor;
+
+    public GameObject ThePlayer;
+    public GameObject TheZombie;
+    public RaycastHit Shot;
+    public bool CanAttack;
+    public bool IsAttacking;
+    public GameObject ScreenFlash;
+    public AudioSource Hurt001;
+    public AudioSource Hurt002;
+    public AudioSource Hurt003;
+    public int PainSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        CanAttack = false;
+        IsAttacking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(ThePlayer.transform);            
+        agent.GetComponent<NavMeshAgent>().speed = 1;
+        if (CanAttack == false)
+        {
+            //TheZombie.GetComponent<Animation>().Play("walk");
+            agent.SetDestination(ThePlayer.transform.position);
+        }
+        
+        if (CanAttack == true)
+        {
+            if (IsAttacking == false)
+            {
+                StartCoroutine(Attack());
+            }
+            agent.GetComponent<NavMeshAgent>().speed = 0;
+            //TheZombie.GetComponent<Animation>().Play("attack");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "ZombieTrigger")
+        {
+            CanAttack = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "ZombieTrigger")
+        {
+            CanAttack = false;
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        IsAttacking = true;
+
+        if (healthMonitor.StalwartActive == false)
+        {
+            PainSound = Random.Range(1, 4);
+            yield return new WaitForSeconds(0.9f);
+            ScreenFlash.SetActive(true);
+            GlobalHealth.PlayerHealth -= 20;
+            if (PainSound == 1)
+            {
+                Hurt001.Play();
+            }
+            else if (PainSound == 2)
+            {
+                Hurt002.Play();
+            }
+            else if (PainSound == 3)
+            {
+                Hurt003.Play();
+            }
+            yield return new WaitForSeconds(0.05f);
+            ScreenFlash.SetActive(false);
+        }
+        else if (healthMonitor.StalwartActive == true)
+        {
+            yield return new WaitForSeconds(0.9f);
+            healthMonitor.CurrentArmour -= 1;
+        }
+        yield return new WaitForSeconds(1);
+        IsAttacking = false;
+    }
+}
---
+++ FPS attempt/Assets/Scripts/Zombies/ZombieHealth.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieHealth : MonoBehaviour
+{
+    public GlobalZombie globalZombie;
+
+    public int CurrentHealth;
+    public GameObject TheZombie;
+    public NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start()
+    {
+        CurrentHealth = 100;
+    }
+
+    void DealDamage(int GunDamage)
+    {
+        CurrentHealth -= GunDamage;
+        GlobalScore.CurrentScore += 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurrentHealth <= 0)
+        {
+            TheZombie.GetComponent<ZombieAI>().enabled = false;
+            agent.GetComponent<NavMeshAgent>().speed = 0;
+            //TheZombie.GetComponent<Animation>().Play("Dying");
+            TheZombie.GetComponent<CapsuleCollider>().enabled = false;
+            StartCoroutine(EndZombie());
+
+        }
+    }
+
+    IEnumerator EndZombie()
+    {
+        yield return new WaitForSeconds(3);
+        GlobalScore.CurrentScore += 60;
+        globalZombie.ZombieCount -= 1;
+        globalZombie.RoundLimit -= 1;
+        Destroy(gameObject);
+    }
+
+}
